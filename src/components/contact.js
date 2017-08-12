694b99caa867ad21e81c859f09e6f5f67c7edd64
gHashTag <raoffonom@icloud.com>
2017-08-12 03:10:41 +0300

    map

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
+
+const AnyReactComponent = ({ text }) => (
+  <div style={{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#BF813E', 
+    background: '#121111',
+    height: 40, width: 115    
+  }}>
+  <h3 style={{alignSelf: 'center' }}>{text}</h3>
+  </div>
+)
 
 export default class Contact extends Component {
+  static defaultProps = {
+    center: {lat: 55.757, lng: 37.5555},
+    zoom: 17
+  }
   render() {
     return (
       <div className='App'>
         <p className='App-intro'>
            Контакт 
         </p>
+       <GoogleMapReact
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+      >
+        <AnyReactComponent
+          lat={55.75777}
+          lng={37.5549}
+          text={'Studio9Reborn'}
+         /> 
+      </GoogleMapReact>
       </div>
     )
   }
--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Restaurant extends Component {
   render() {
     return (
-      <div className='App'>
+     <div className='App'>
         <p className='App-intro'>
           Ресторан 
         </p>
